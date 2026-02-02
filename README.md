3a8790d490fac4e708f7ef61c8ae31518066f96e
NMSaiTharun <nmsaitarun@gmail.com>
2026-02-02 16:16:21 +0000

    Added package.json, node_modules and some ReactElement code.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 #Namste React Learning!
+
+# Parcel
+- Dev Build
+- Local server
+- HMR=> Hot Module Replacement
+- File watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimisation
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - Remove unused code
+- Different dev and production bundles
--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-/* const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+const heading=React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React!");
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-console.log(heading); */
+console.log(heading); 
 const parent=React.createElement("div"
 ,{id:"parent"},[
     React.createElement(
-    "div",{id:"child1"}
-    ,[React.createElement("h1",{id:"heading"},"Iam h1 tag"),React.createElement("h2",{id:"heading2"},"Im h2 tag")]),
+    "div",{"id":"child1",key:"child1"}
+    ,[React.createElement("h1",{"id":"heading1",key:"h-1"},"Iam h1 tag"),React.createElement("h2",{id:"heading2",key:"h-2"},"Im h2 tag")]),
     React.createElement(
-    "div",{id:"child2"}
-    ,[React.createElement("h1",{id:"heading"},"Iam h1 tag"),React.createElement("h2",{id:"heading2"},"Im h2 tag")])]
+    "div",{id:"child2",key:"child2"}
+    ,[React.createElement("h1",{id:"heading3",key:"h-1-3"},"Iam h1 tag"),React.createElement("h2",{id:"heading4",key:"h-1-4"},"Im h2 tag")])]
 );
-const root=ReactDOM.createRoot(document.getElementById("root"));
+//const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
