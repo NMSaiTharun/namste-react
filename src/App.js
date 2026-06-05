99bfccca3a09fd1e8e8ec42d274a88e16d2cd68c
NMSaiTharun <nmsaitarun@gmail.com>
2026-06-05 17:32:52 +0100

    added useState(React Hook)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Header from "./src/components/Header";
-import Body from "./src/components/Body";
+import Header from "./components/Header";
+import Body from "./components/Body";
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading", xyz: "abc" },
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resObj from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () => {
+  const [restaurantObject, setRestaurantObject] = useState(resObj);
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log(restaurantObject);
+            let filteredRestaurantObject = restaurantObject.filter(
+              (a) => a.card.card.info.avgRating > 4,
+            );
+            console.log(filteredRestaurantObject);
+            setRestaurantObject(filteredRestaurantObject);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div
         className="res-container"
         style={{ display: "flex", flexWrap: "wrap" }}
       >
-        {resObj.map((restaurant, index) => (
+        {restaurantObject.map((restaurant, index) => (
           <RestaurantCard
             key={restaurant.card.card.info.id}
             resData={restaurant}
