02e9840ce2b1239832fd419861ae741de2a34cbf
NMSaiTharun <nmsaitarun@gmail.com>
2026-06-07 13:57:34 +0100

    tharun-react-router-dom, routing, Link tag

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>This is a React project for learning!</h1>
+    </div>
+  );
+};
---
+++ src/components/Contact.js
@@ -0,0 +1,7 @@
+export const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Sai Tharun!</h2>
+    </div>
+  );
+};
