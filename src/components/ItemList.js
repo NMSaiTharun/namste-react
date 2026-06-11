f0debb3f6b5a6a264e90196618fafae2da82bc7d
NMSaiTharun <nmsaitarun@gmail.com>
2026-06-11 17:00:06 +0100

    menu-items-UI-changes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,5 +1,71 @@
-const ItemList = (items) => {
+import { CDN_URL } from "../utils/constants";
+const getRatingColor = (rating) => {
+  if (rating >= 4) return "text-green-800";
+  if (rating >= 3) return "text-green-400";
+  return "text-yellow-400";
+};
+
+const ItemList = ({ items }) => {
   console.log(items);
-  return <div>ItemList</div>;
+  return (
+    <div>
+      <div>
+        {items.map((item) => (
+          <div
+            key={item.card.info.id}
+            className="py-2 my-2 border-b-4 border-gray-200 text-left"
+          >
+            <div className="flex justify-between">
+              <div className="p-2 flex flex-col">
+                {item.card.info.isVeg ? (
+                  <span className="w-5 h-5 border-2 border-green-600 flex items-center justify-center">
+                    <span className="w-2.5 h-2.5 rounded-full bg-green-600"></span>
+                  </span>
+                ) : (
+                  <span className="w-5 h-5 border-2 border-red-600 flex items-center justify-center">
+                    <span className="text-red-600 text-xs">▲</span>
+                  </span>
+                )}
+
+                <span className="font-bold text-2xl">
+                  {item.card.info.name}
+                </span>
+                <span className="text-xl font-semibold">
+                  ₹{(item.card.info.price || item.card.info.defaultPrice) / 100}
+                </span>
+                <div className="flex">
+                  <span
+                    className={`${getRatingColor(item.card.info.ratings?.aggregatedRating?.rating)} flex items-center gap-1 text-lg font-semibold`}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-4 h-4"
+                    >
+                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87L18.18 21 12 17.77 5.82 21 7 14.14l-5-4.87 6.91-1.01L12 2z" />
+                    </svg>
+                    {item.card.info.ratings?.aggregatedRating?.rating}
+                  </span>
+                  <span className="text-gray-600 pt-0.5 pl-1">
+                    ({item.card.info.ratings?.aggregatedRating?.ratingCountV2})
+                  </span>
+                </div>
+              </div>
+              <div className="w-3/12">
+                <img
+                  className="rounded-lg"
+                  src={CDN_URL + item.card.info.imageId}
+                />
+              </div>
+            </div>
+            <p className="font-monospace text-[16px] text-gray text-lg">
+              {item.card.info.description}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default ItemList;
--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -6,13 +6,13 @@ const RestaurantCategory = (props) => {
       <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
         <div className="flex justify-between">
           {" "}
-          <span className="font-bold text-lg">
-            {categories.title} ({categories.itemCards.length})
+          <span className="font-bold text-2xl">
+            {categories?.title} ({categories?.itemCards?.length})
           </span>
-          <span>⬇️</span>
+          <span className="text-3xl">⬇️</span>
         </div>
 
-        <ItemList items={categories.itemCards} />
+        <ItemList items={categories?.itemCards || []} />
       </div>
     </div>
   );
